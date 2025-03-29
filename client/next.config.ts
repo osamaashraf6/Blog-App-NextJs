0215a19bf0d3166c69a69a48ce11807424820424
osamaashraf6 <osamaashraf668@gmail.com>
2025-03-29 22:55:29 +0200

    Fix The Deployment Issue ++4

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-        // pathname: "/users/**", // Adjust this based on your image path
-      },
-      {
         protocol: "https",
         hostname: "encrypted-tbn0.gstatic.com",
         pathname: "/images/**", // Adjust if needed
